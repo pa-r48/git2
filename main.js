735b19845bc424afae88f1ada83f0643cb4445b7
pa-r48 <pasqualepioradatti2@gmail.com>
2025-04-08 10:34:46 +0200

    commit 2

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 import PromptSync from "prompt-sync"
 const prompt = PromptSync();
 
-let x = prompt("numero: ");
-console.log(parseInt(x) + parseInt(x));
+let x = Number(prompt("primo addendo: "));
+let y = Number(prompt("secondo addendo: "));
+
+let segno = prompt("inserire l'operazione (+ - * /): ");
+
+switch (segno){
+    case "+":
+        console.log(x+y);
+        break;
+
+    case "-":
+        console.log(x-y);
+        break;
+
+    case "*":
+        console.log(x*y);
+        break;
+
+    case "/":
+        console.log(x/y);
+        break;
+}
